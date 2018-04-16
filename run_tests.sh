cdd89fd29ab3c091d7c17b6aa74be06f948060f6
Matthias Noback <matthiasnoback@gmail.com>
2018-04-16 15:18:43 +0200

    Only run unit tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ set -e
 
 vendor/bin/phpstan analyse -l max src test
 vendor/bin/phpunit --testsuite unit -v
-vendor/bin/phpunit --testsuite integration -v
-vendor/bin/behat --suite acceptance -vvv
+#vendor/bin/phpunit --testsuite integration -v
+#vendor/bin/behat --suite acceptance -vvv
