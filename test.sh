8cb1b0f9fe48c104562e653e03d26445a56fea15
Matthias Noback <matthiasnoback@gmail.com>
2023-05-11 08:54:45 +0200

    No separate unit and integration tests

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 set -e
 
 vendor/bin/phpstan analyse
-vendor/bin/phpunit --testsuite unit -v
-vendor/bin/phpunit --testsuite integration -v
-vendor/bin/behat --suite acceptance -vvv
+vendor/bin/phpunit -v
+#vendor/bin/behat --suite acceptance -vvv
