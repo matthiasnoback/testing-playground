df4a51beac1cd98929d989a4daf8606cd8c8d2f0
Matthias Noback <matthiasnoback@gmail.com>
2023-09-08 12:13:12 +0200

    Slice 3: discount

--- src/Calculator/Calculator.php
+++ src/Calculator/Calculator.php
@@ -6,6 +6,23 @@ final class Calculator
 {
     public function calculate(int $quantity, float $amount): float
     {
-        return $quantity * $amount;
+        $totalPrice = $quantity * $amount;
+
+        $discount = $this->getDiscount($totalPrice);
+
+        return $totalPrice * (100 - $discount) / 100;
+    }
+
+    public function getDiscount(float $totalPrice): int
+    {
+        if ($totalPrice >= 200) {
+            return 10;
+        }
+
+        if ($totalPrice >= 100) {
+            return 5;
+        }
+
+        return 0;
     }
 }
--- test/Foo/CalculatorTest.php
+++ test/Foo/CalculatorTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Foo;
 
+use Calculator\Calculator;
 use PHPUnit\Framework\TestCase;
 
 final class CalculatorTest extends TestCase
@@ -27,5 +28,19 @@ final class CalculatorTest extends TestCase
         $lastLine = exec('php calculator.php 5 10,10');
 
         self::assertEquals('50,50', $lastLine);
+    }
+
+    public function test_amount_is_100(): void
+    {
+        $this->assertEquals(95.0, (new Calculator())->calculate(10, 10.0));
+    }
+
+    public function test_get_discount(): void
+    {
+        $this->assertEquals(0, (new Calculator())->getDiscount(95));
+        $this->assertEquals(5, (new Calculator())->getDiscount(100));
+        $this->assertEquals(5, (new Calculator())->getDiscount(150));
+        $this->assertEquals(10, (new Calculator())->getDiscount(200));
+        $this->assertEquals(10, (new Calculator())->getDiscount(300));
     }
 }
